d717b0868bdcec20a37c7d7a283b4eb4e081d8e4
baduke3520 <luch0002@stud.kea.dk>
2026-02-18 11:31:43 +0100

    inital

---
+++ src/main/java/dk/ek/shift_happens/ShiftHappensApplication.java
@@ -0,0 +1,13 @@
+package dk.ek.shift_happens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftHappensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiftHappensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dk/ek/shift_happens/ShiftHappensApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.ek.shift_happens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftHappensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
